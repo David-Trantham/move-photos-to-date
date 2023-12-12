006f80a735904481e33d3d8be482acff8ffef1b0
Green <greeniscool1234@gmail.com>
2023-12-11 22:14:39 -0500

    Add counting files feature and increased usability

--- script.bat
+++ script.bat
@@ -1,6 +1,13 @@
 @echo off
 set "srcpath=C:\Users\green\OneDrive\Desktop\move-photos-to-date\input"
 set "destpath=C:\Users\green\OneDrive\Desktop\move-photos-to-date\output"
+
+echo Scanning.....
+dir %srcpath% /b /a-d | find /c /v ""
+echo Photos have been found within the folder found at: %srcpath% 
+
 set /P "destfolder=Please enter the date for the folder to move the photos to:"
 mkdir "%destpath%\%destfolder%"
 move "%srcpath%\*" "%destpath%\%destfolder%\"
+
+pause
