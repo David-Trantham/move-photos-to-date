1eb8090c1083a9399e06cde1bdd33d638e47e444
Green <greeniscool1234@gmail.com>
2023-12-18 13:49:01 -0500

    Working on branching

--- Move Santa Photos Script.bat
+++ Move Santa Photos Script.bat
@@ -1,17 +1,18 @@
 @echo off
-::These two paths may need to be updated in the future:
-set "srcpath=C:\Users\green\OneDrive\Desktop\move-photos-to-date\input"
-set "destpath=C:\Users\green\OneDrive\Desktop\move-photos-to-date\output"
-
 echo Scanning.....
 dir %srcpath% /b /a-d | find /c /v ""
-echo Photos have been found within the folder found at: %srcpath% 
+echo Photos have been found within the source folder.
+
+:main
+set /P "destfolder=Please enter the date for the folder to move the photos to, or enter 'p' to view and modify paths.
+
+:path_menu
 
 ::Maybe use a "Choice" to determine wether or not we are modifying the paths?
-Choice /C p /T 10 /M "Press ""p"" to view or modify paths, or press any other key to continue..."
+Choice /C BSD /T 10 /M "Press ""p"" to view or modify paths, or press any other key to continue..."
 IF %ERRORLEVEL% EQU 1 goto path_menu
 
-set /P "destfolder=Please enter the date for the folder to move the photos to, or enter 'p' to view and modify paths.
+
 
 mkdir "%destpath%\%destfolder%"
 move "%srcpath%\*" "%destpath%\%destfolder%\"
