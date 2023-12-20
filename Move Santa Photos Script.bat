c451186cdb137c2274102efa2bdaab47807c97c3
Green <greeniscool1234@gmail.com>
2023-12-19 22:45:56 -0500

    Added pause and exit, among other upgrades

--- Move Santa Photos Script.bat
+++ Move Santa Photos Script.bat
@@ -1,14 +1,15 @@
 @echo off
+title "Move Santa Photos Utility"
 :main
 echo Scanning.....
+::TODO: I don't like that this is is potentially calling srcpath before it is set 
 dir %srcpath% /b /a-d | find /c /v ""
 echo Photos have been found within the source folder.
 
 :main_prompt
 set /P "destfolder=Please enter the date for the folder to move the photos to, enter 'p' to view and modify paths, or enter 'q' to quit:"
 if "%destfolder%" == "p" (goto path_menu)
-if "%destfolder%" == "q" (exit \b)
-::Need to test, but this should check if destfolder contains only numbers; this is what should activate the rest of the script
+if "%destfolder%" == "q" (exit /b)
 set "var="&for /f "delims=0123456789\/-_" %%i in ("%destfolder%") do set "var=%destfolder%"%
 if defined var (echo Error: Command or Date not recognized.&goto main_prompt)
 if "%srcpath%" == "" (echo Error: Please set a source path, or try closing and re-launching the program.& goto main_prompt)
@@ -16,10 +17,10 @@ if "%destpath%" == "" (echo Error: Please set a destination path, or try closing
 ::If destfolder is a number or valid symbol, and both paths exist...
 mkdir "%destpath%\%destfolder%"
 move "%srcpath%\*" "%destpath%\%destfolder%\"
-
+pause
+exit /b
 :path_menu
 ::TODO: Display and explain paths here
-::Maybe use a "Choice" to determine wether or not we are modifying the paths?
 Choice /C SDB /M "Please press 'S' to modify the source path, 'D' to modify the destination path, or press 'B' to go back.
 IF %ERRORLEVEL% EQU 1 goto update_source
 IF %ERRORLEVEL% EQU 2 goto update_destination
@@ -29,12 +30,12 @@ IF %ERRORLEVEL% EQU 3 goto main & cls
 set /P "input=Please paste or enter the desired source path: "
 if not exist %input% (echo Error: Path not found. Please enter a valid path. & goto path_menu)
 setx srcpath %input%
-echo Success! Source path has been updated.
+echo Success! Source path has been updated. Note that you need to close and re-launch the program to finalize changes.
 goto path_menu
 
 :update_destination
 set /P "input=Please paste or enter the desired destination path: "
 if not exist %input% (echo Error: Path not found. Please enter a valid path. & goto path_menu)
 setx destpath %input%
-echo Success! Destination path has been updated.
+echo Success! Destination path has been updated. Note that you need to close and re-launch the program to finalize changes.
 goto path_menu
